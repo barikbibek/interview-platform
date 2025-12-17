7a971b79a8db54dd5e4b5d9cefb7e5e263e9c472
bibek <devmail.js@gmail.com>
2025-12-17 16:40:23 +0530

    small fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env';
 
 const app = express()
 
-app.get('/', (req, res) => {
+const ROOT_DIR = process.cwd()
+
+app.get('/health', (req, res) => {
     res.send("hello")
 })
+
+
+if(ENV.NODE_ENV === "production"){
+    const filePath = path.join(ROOT_DIR, 'public/dist')
+    app.use(express.static(filePath))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(filePath, "index.html"))
+    })
+}else{
+    app.get("/{*any}", (req, res) => {
+        res.send('Development mode')
+    })
+}
 
 app.listen(ENV.PORT , () => console.log(`Server is listenning on PORT: ${ENV.PORT}`))
